b4a47c21021b1aaf8d31121aaa2d69529251b253
maxwe45 <maxwell.weisz@gmail.com>
2024-04-20 13:14:38 -0400

    Added some more code to allow books to be displayed after being entered. Have to style the site and finish all functions of the site.

--- library.js
+++ library.js
@@ -5,7 +5,7 @@ const closeForm = document.querySelector('#close');
 const submitBttn = document.querySelector('#submit');
 const library = document.querySelector('.library');
 
-const myLibrary = [];
+let myLibrary = [];
 
 //Constructor for book takes title,author,pages,if its been read
 function Book(name,author,pages,beenRead) {
@@ -16,13 +16,54 @@ function Book(name,author,pages,beenRead) {
 };
 //Function to add a book to a library using the form in the website
 function addBookToLibrary() {
-    const title = document.getElementById('name').value;
-    const author = document.getElementById('author').value;
-    const pages = document.getElementById('pages').value;
-    const hasBeenRead = document.querySelector('#hasBeenRead').checked;
-    const newBook = new Book(title,author,pages,hasBeenRead);
+    let title = document.getElementById('name').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let hasBeenRead = document.querySelector('#hasBeenRead').checked;
+    let newBook = new Book(title,author,pages,hasBeenRead);
     myLibrary.push(newBook)
+};
+
+const removeBook = book => {
+    myLibrary = myLibrary.filter(() => this.name !== book.name,Book);
 }
+
+const display = () => {
+    let book = myLibrary[myLibrary.length - 1];
+    const libraryEl = document.querySelector('.library');
+    let div = document.createElement('div');
+    div.classList.add('card');
+    libraryEl.appendChild(div);
+
+    //Declares a delete button for the book added 
+    let delBttn = document.createElement('button');
+    delBttn.classList.add('deleteBttn');
+    delBttn.textContent = 'Remove';
+    div.appendChild(delBttn);
+    //Removes the book from the myLibrary array and the div html element
+    delBttn.addEventListener('click', () => {
+        removeBook(book);
+        div.remove();
+    })
+    
+    let bookTitle = document.createElement('p');
+    let bookAuthor = document.createElement('p');
+    let bookPages = document.createElement('p');
+    let read = document.createElement('button');
+    bookTitle.classList.add('bookTitle');
+    bookAuthor.classList.add('bookAuthor');
+    bookPages.classList.add('bookPages');
+    read.classList.add('bookRead');
+
+    bookTitle.textContent = book.name;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = book.pages;
+    div.appendChild(bookTitle);
+    div.appendChild(bookAuthor);
+    div.appendChild(bookPages);
+    
+};
+
 //Button to show the add book form
 showForm.addEventListener('click', () => {
     addBookForm.show()
@@ -34,5 +75,5 @@ closeForm.addEventListener('click', ()=> {
 //Submit button adds the submitted form info to myLibrary using addBookToLibrary
 submitBttn.addEventListener('click', ()=> {
     addBookToLibrary();
-
+    display()
 });
