c9c9c3858e208bab73e3162c4afff744ad8924ae
maxwe45 <maxwell.weisz@gmail.com>
2024-04-21 10:22:20 -0400

    Added the functionality of the read button so I believe all functionality of the site is done. Will update if any bugs are found, but going to work on styling site to be more usable.

--- library.js
+++ library.js
@@ -4,7 +4,7 @@ const showForm = document.querySelector('#showForm');
 const closeForm = document.querySelector('#close');
 const submitBttn = document.querySelector('#submit');
 const library = document.querySelector('.library');
-
+//Array of books that are displayed on page
 let myLibrary = [];
 
 //Constructor for book takes title,author,pages,if its been read
@@ -21,13 +21,19 @@ function addBookToLibrary() {
     let pages = document.getElementById('pages').value;
     let hasBeenRead = document.querySelector('#hasBeenRead').checked;
     let newBook = new Book(title,author,pages,hasBeenRead);
-    myLibrary.push(newBook)
+    myLibrary.push(newBook);
 };
-
-const removeBook = book => {
-    myLibrary = myLibrary.filter(() => this.name !== book.name,Book);
+//Function for the delete button on books to remove that book from the library
+const removeBook = badBook => {
+    myLibrary = myLibrary.filter(book => book.name !== badBook,Book);
+};
+//Prototype for Book constructor to toggle the status of read on the read button for each book
+Book.prototype.toggleRead =  function () {
+    this.beenRead = !this.beenRead
 }
 
+//Function to display the books that are added by user, with addition of button to toggle its read status and remove it from the 
+//library array as well as the book on the page display.
 const display = () => {
     let book = myLibrary[myLibrary.length - 1];
     const libraryEl = document.querySelector('.library');
@@ -42,36 +48,54 @@ const display = () => {
     div.appendChild(delBttn);
     //Removes the book from the myLibrary array and the div html element
     delBttn.addEventListener('click', () => {
-        removeBook(book);
+        removeBook(book.name);
         div.remove();
     })
-    
+    // html elements to display book info
     let bookTitle = document.createElement('p');
     let bookAuthor = document.createElement('p');
     let bookPages = document.createElement('p');
     let read = document.createElement('button');
+    // html classes for the html elements
     bookTitle.classList.add('bookTitle');
     bookAuthor.classList.add('bookAuthor');
     bookPages.classList.add('bookPages');
     read.classList.add('bookRead');
-
+    // html element text contents
     bookTitle.textContent = book.name;
     bookAuthor.textContent = book.author;
     bookPages.textContent = book.pages;
+
+    //Read button that toggles the read status with the Book prototype toggleread and readText to update the button text content
+    read.addEventListener('click', () => {
+        book.toggleRead();
+        readText();
+    })
+    //Shows read buttons text content as either read or not and adds to a seperate class
+    let readText = () =>{
+        if(book.beenRead === true){
+            read.textContent = 'Read';
+            read.classList.add('bookReadTrue');
+        }
+        else{
+            read.textContent = 'Not Read'}};
+            read.classList.add('bookReadFalse')
+    readText();
+    
     div.appendChild(bookTitle);
     div.appendChild(bookAuthor);
     div.appendChild(bookPages);
-    
+    div.appendChild(read);
 };
 
 //Button to show the add book form
 showForm.addEventListener('click', () => {
     addBookForm.show()
-})
+});
 //Button to close the add book form
 closeForm.addEventListener('click', ()=> {
     addBookForm.close()
-})
+});
 //Submit button adds the submitted form info to myLibrary using addBookToLibrary
 submitBttn.addEventListener('click', ()=> {
     addBookToLibrary();
